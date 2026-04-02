2c72a00f746e1ecb2136f565893cc44e3c7878a5
Adz-Nelo <adz.nelo155@gmail.com>
2026-04-02 15:52:47 +0800

    added input elements and flip direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add gravity when not on the floor
 	if not is_on_floor():
@@ -11,8 +13,25 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Get direction
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Play animation
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
+	# Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
